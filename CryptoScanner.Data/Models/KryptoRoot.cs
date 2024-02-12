d744fc559ad34a08bd364ce549049055f23f42c9
Alinia93 <alinia93@gmail.com>
2024-02-12 15:49:25 +0100

    Made Krypto Model And ViewModel

---
+++ CryptoScanner.Data/Models/KryptoRoot.cs
@@ -0,0 +1,37 @@
+﻿namespace CryptoScanner.Data.Models
+{
+    public class KryptoRoot
+    {
+        public class Root
+        {
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+
+
+            [JsonProperty("current_price")]
+            public int CurrentPrice { get; set; }
+
+            [JsonProperty("market_cap")]
+            public long MarketCap { get; set; }
+
+
+            [JsonProperty("ath_date")]
+            public DateTime AthDate { get; set; }
+
+
+            [JsonProperty("atl_date")]
+            public DateTime AtlDate { get; set; }
+
+
+
+            [JsonProperty("last_updated")]
+            public DateTime LastUpdated { get; set; }
+        }
+
+    }
+}
