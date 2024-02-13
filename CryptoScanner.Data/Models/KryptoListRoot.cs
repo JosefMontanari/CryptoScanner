93c68be143643c7b2e3bea3e89c56004377cd732
Alinia93 <alinia93@gmail.com>
2024-02-13 20:47:03 +0100

    hello

---
+++ CryptoScanner.Data/Models/KryptoListRoot.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoScanner.Data.Models
+{
+    public class KryptoListRoot
+    {
+
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+    }
+}
