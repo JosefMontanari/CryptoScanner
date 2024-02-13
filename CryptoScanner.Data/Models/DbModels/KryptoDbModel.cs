2333af764a445a73df6f69255f0a0f45fa31c15f
Alinia93 <alinia93@gmail.com>
2024-02-13 09:31:40 +0100

    madeDbModelsOsv

---
+++ CryptoScanner.Data/Models/DbModels/KryptoDbModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoScanner.Data.Models.DbModels
+{
+    public class KryptoDbModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int CurrentPrice { get; set; }
+    }
+}
--- CryptoScanner.Data/Models/ViewModel.cs
+++ CryptoScanner.Data/Models/ViewModel.cs
@@ -2,9 +2,9 @@
 {
     public class ViewModel
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public int MyProperty { get; set; }
+
 
         public int CurrentPrice { get; set; }
 
