4b74fea9f1ef94bbc14f21b09a75cde60d25e45c
JosefMontanari <josef.montanari94@gmail.com>
2024-02-13 11:23:40 +0100

    Some stuff

--- CryptoScanner.UI/Pages/Index.cshtml.cs
+++ CryptoScanner.UI/Pages/Index.cshtml.cs
@@ -1,15 +1,13 @@
-using CryptoScanner.App.Api;
 using CryptoScanner.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using static CryptoScanner.Data.Models.KryptoRoot;
 
 namespace CryptoScanner.UI.Pages
 {
     public class IndexModel : PageModel
     {
         public List<ViewModel> Currencies { get; set; }
-        public Root? CryptoCurrencyRootModel { get; set; }
+        public ViewModel? CryptoCurrencyRootModel { get; set; }
         public string ErrorMessage { get; set; }
 
         [BindProperty]
@@ -30,7 +28,7 @@ namespace CryptoScanner.UI.Pages
             try
             {
                 // Sök på coinet med hjälp av namn för att få upp värdet
-                ViewModel viewModel = await new Apicaller().GetBitcoin(CryptoCurrencyName);
+                //CryptoCurrencyRootModel = await new Apicaller().GetBitcoin(CryptoCurrencyName);
             }
             catch (Exception ex)
             {
